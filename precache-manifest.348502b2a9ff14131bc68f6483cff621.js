3c29b85065a9c69512efbba58351e4c100947762
Noah Lopez <noahtheark2@gmail.com>
2021-02-24 20:23:29 -0800

    Updates

---
+++ precache-manifest.348502b2a9ff14131bc68f6483cff621.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03bb93a619f86baaa136b8b2fed15b8",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "c1a0c7260c033f2e8bf0",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "094347c180b948120ae6",
+    "url": "/face-recognition-brain/static/css/main.abbdd266.chunk.css"
+  },
+  {
+    "revision": "c1a0c7260c033f2e8bf0",
+    "url": "/face-recognition-brain/static/js/2.5fc42434.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-brain/static/js/2.5fc42434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "094347c180b948120ae6",
+    "url": "/face-recognition-brain/static/js/main.9ea39ba8.chunk.js"
+  },
+  {
+    "revision": "56148025b8b7094fd937",
+    "url": "/face-recognition-brain/static/js/runtime-main.fa010191.js"
+  },
+  {
+    "revision": "dcfe64ed5a613b058359a9aca76ca60f",
+    "url": "/face-recognition-brain/static/media/cool-background.dcfe64ed.png"
+  }
+]);
